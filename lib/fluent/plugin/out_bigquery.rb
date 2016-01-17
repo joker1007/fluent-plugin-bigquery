ec4b10297169f4c9d959db0c33a838b688a3d98d
joker1007 <kakyoin.hierophant@gmail.com>
2016-01-17 17:49:46 +0900

    Switch default parameter by `method` type

--- lib/fluent/plugin/out_bigquery.rb
+++ lib/fluent/plugin/out_bigquery.rb
@@ -19,19 +19,26 @@ module Fluent
     # https://developers.google.com/bigquery/browser-tool-quickstart
     # https://developers.google.com/bigquery/bigquery-api-quickstart
 
-    config_set_default :buffer_type, 'lightening'
+    ### default for insert
+    def configure_for_insert(conf)
+      raise ConfigError unless conf["method"] != "load"
 
-    config_set_default :flush_interval, 0.25
-    config_set_default :try_flush_interval, 0.05
+      conf["buffer_type"]                = "lightening"  unless conf["buffer_type"]
+      conf["flush_interval"]             = 0.25          unless conf["flush_interval"]
+      conf["try_flush_interval"]         = 0.05          unless conf["try_flush_interval"]
+      conf["buffer_chunk_limit"]         = 1 * 1024 ** 2 unless conf["buffer_chunk_limit"] # 1MB
+      conf["buffer_queue_limit"]         = 1024          unless conf["buffer_queue_limit"]
+      conf["buffer_chunk_records_limit"] = 500           unless conf["buffer_chunk_records_limit"]
+    end
 
-    config_set_default :buffer_chunk_records_limit, 500
-    config_set_default :buffer_chunk_limit, 1000000
-    config_set_default :buffer_queue_limit, 1024
+    ### default for loads
+    def configure_for_load(conf)
+      raise ConfigError unless conf["method"] == "load"
 
-    ### for loads
-    ### TODO: different default values for buffering between 'load' and insert
-    # config_set_default :flush_interval, 1800 # 30min => 48 imports/day
-    # config_set_default :buffer_chunk_limit, 1000**4 # 1.0*10^12 < 1TB (1024^4)
+      # buffer_type, flush_interval, try_flush_interval is TimeSlicedOutput default
+      conf["buffer_chunk_limit"] = 1 * 1024 ** 3 unless conf["buffer_chunk_limit"] # 1GB
+      conf["buffer_queue_limit"] = 64            unless conf["buffer_queue_limit"]
+    end
 
     ### OAuth credential
     # config_param :client_id, :string
@@ -173,6 +180,11 @@ module Fluent
     end
 
     def configure(conf)
+      if conf["method"] == "load"
+        configure_for_load(conf)
+      else
+        configure_for_insert(conf)
+      end
       super
 
       if @method == "insert"
